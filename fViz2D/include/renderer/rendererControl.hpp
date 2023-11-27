6c2ea632926b29ae5144370036b79641a8d53f08
eu <en.gustavo@gmail.com>
2023-11-27 08:24:33 -0300

    v0.3.4 #1 - Resolution choice in the API

--- fViz2D/include/renderer/rendererControl.hpp
+++ fViz2D/include/renderer/rendererControl.hpp
@@ -9,12 +9,12 @@ namespace F_V2 {
 	//Supported formats are the same as those supported by the texture loading functions in renderData.hpp
 	F_V2::rendererRetCode_st rendererMain(bool* externalBool_ptr, IMG::generic2DfieldPtr_t* dynamicData_ptr, 
 		             COLOR::rgbaF_t* clearColor_ptr, COLOR::rgbaF_t* noiseTint_ptr,
-		             const char* bannerPathFromBinary);
+		             int width, int height, const char* bannerPathFromBinary);
 
 	//Sets returnCode_ptr to BAD_DYNAMIC_DATA_FORMAT if dynamicData_ptr's format is not supported, otherwise to OK
 	//Supported formats are the same as those supported by the texture loading functions in renderData.hpp
 	void rendererMainForSeparateThread(bool* externalBool_ptr, IMG::generic2DfieldPtr_t* dynamicData_ptr, 
 									   COLOR::rgbaF_t* clearColor_ptr, COLOR::rgbaF_t* noiseTint_ptr, 
 		                               F_V2::rendererRetCode_st* returnCode_ptr, 
-		                               const char* bannerPathFromBinary);
+		                               int width, int height, const char* bannerPathFromBinary);
 }
